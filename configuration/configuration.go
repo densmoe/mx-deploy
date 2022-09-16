eaabe089ad6d44902b197490aeb40b3ca96aef33
Dennis Möller <dennis.moller@mendix.com>
2022-09-16 17:34:15 +0200

    envs

--- configuration/configuration.go
+++ configuration/configuration.go
@@ -4,6 +4,8 @@ type Configuration struct {
 	DeployAPIBaseURL  string `env:"DEPLOY_API_BASE_URL" envDefault:"https://deploy.mendix.com/api/1"`
 	DeployAPIUsername string `env:"DEPLOY_API_USERNAME"`
 	DeployAPIKey      string `env:"DEPLOY_API_KEY"`
+	AppId             string `env:"DEPLOY_API_APP_ID"`
+	ProjectId         string `env:"DEPLOY_API_PROJECT_ID"`
 	DebugMode         bool   `env:"DEBUG_MODE" envDefault:true`
 }
 
