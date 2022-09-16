88f5b4186f2f22b69558576d6985f91b5c177db5
Dennis Möller <dennis.moller@mendix.com>
2022-09-16 16:58:59 +0200

    envs

--- deploy_api/deploy_api.go
+++ deploy_api/deploy_api.go
@@ -21,6 +21,16 @@ type App struct {
 	AppId     string `json:"AppId"`
 }
 
+type Environment struct {
+	Status        string `json:"Status"`
+	EnvironmentId string `json:"EnvironmentId"`
+	Mode          string `json:"Mode"`
+	Url           string `json:"Url"`
+	ModelVersion  string `json:"ModelVersion"`
+	MendixVersion string `json:"MendixVersion"`
+	IsProduction  bool   `json:"Production"`
+}
+
 func (d DeployAPI) RetrieveApps() []App {
 	client := http.Client{}
 	req, _ := http.NewRequest("GET", d.BaseURL+"/apps", nil)
@@ -51,4 +61,20 @@ func (d DeployAPI) RetrieveApp(appId string) App {
 		log.Error(err)
 	}
 	return app
+}
+
+func (d DeployAPI) RetrieveEnvironments(appId string) []Environment {
+	client := http.Client{}
+	req, _ := http.NewRequest("GET", d.BaseURL+"/apps/"+appId+"/environments", nil)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Mendix-Username", d.Username)
+	req.Header.Set("Mendix-ApiKey", d.APIKey)
+	response, _ := client.Do(req)
+	jsonDataFromResp, _ := io.ReadAll(response.Body)
+
+	var environments []Environment
+	if err := json.Unmarshal([]byte(jsonDataFromResp), &environments); err != nil {
+		log.Error(err)
+	}
+	return environments
 }
